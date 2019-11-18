fd6cf63f415c30caae3c4cf30a14a2a5fa35cafd
François de Metz <francois@2metz.fr>
2019-11-18 08:49:51 +0100

    Handle 404.

--- addok_getbyid/__init__.py
+++ addok_getbyid/__init__.py
@@ -1,13 +1,17 @@
 import json
+from falcon.errors import HTTPNotFound
 
 from addok import core
 
 class GetByIdView:
 
     def on_get(self, req, resp, doc_id):
-        result = core.Result.from_id(doc_id)
-        resp.body = json.dumps(result.format())
-        resp.content_type = 'application/json; charset=utf-8'
+        try:
+             result = core.Result.from_id(doc_id)
+             resp.body = json.dumps(result.format())
+             resp.content_type = 'application/json; charset=utf-8'
+        except ValueError:
+             raise HTTPNotFound()
 
 def register_http_endpoint(api):
-  api.add_route('/id/{doc_id}', GetByIdView())
+    api.add_route('/id/{doc_id}', GetByIdView())
